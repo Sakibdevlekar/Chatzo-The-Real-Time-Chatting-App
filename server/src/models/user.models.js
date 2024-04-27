8fef81a30345c00f996d894f1b0cb344c141e27a
sakibdevlekar <sakibdevlekar96@gmail.com>
2024-04-27 15:29:14 +0530

    user schema added

---
+++ server/src/models/user.models.js
@@ -0,0 +1,51 @@
+import mongoose, { Schema, model } from "mongoose";
+import { hash } from "bcrypt";
+
+const schema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        bio: {
+            type: String,
+            required: true,
+        },
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            select: false,
+        },
+        avatar: {
+            public_id: {
+                type: String,
+                required: true,
+            },
+            url: {
+                type: String,
+                required: true,
+            },
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
+
+/**
+ * Runs before the user document is saved to the database.
+ * If the password field is modified, it hashes the password using bcrypt.
+ * @param {import("mongoose").HookNextFunction} next - A callback function to continue the save process.
+ */
+schema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+
+    this.password = await hash(this.password, 10);
+});
+
+export const User = mongoose.models.User || model("User", schema);
