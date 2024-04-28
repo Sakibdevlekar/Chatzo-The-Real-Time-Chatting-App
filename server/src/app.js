532e5fbf586c30f18bb25ebd0fc7b4021a535477
sakibdevlekar <sakibdevlekar96@gmail.com>
2024-04-28 19:11:30 +0530

    route and error handler setup

--- server/src/app.js
+++ server/src/app.js
@@ -1,19 +1,21 @@
 import express from "express";
 import { BASE_URL } from "./constant/config.constant.js";
-import {errorHandler} from "./middlewares/error.middleware.js";
-
+import { errorHandler } from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
 
 /* Route Import */
-
+import { adminRoutes } from "./routes/admin.route.js";
 import { userRoutes } from "./routes/user.route.js";
+import { chatRoutes } from "./routes/chat.route.js";
 
 /* Route use*/
+app.use(`${BASE_URL}/admin`, adminRoutes);
 app.use(`${BASE_URL}/user`, userRoutes);
-
-
+app.use(`${BASE_URL}/chat`, chatRoutes);
 
 app.use(errorHandler);
 
