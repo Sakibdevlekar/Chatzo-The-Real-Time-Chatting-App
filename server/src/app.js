a19847eba72a083c1dadf1e0a8f665f58b45aacc
sakibdevlekar <sakibdevlekar96@gmail.com>
2024-05-09 16:51:43 +0530

    swager setup and access controller header set

--- server/src/app.js
+++ server/src/app.js
@@ -3,12 +3,29 @@ import { BASE_URL } from "./constant/config.constant.js";
 import { corsOptions } from "./constant/config.constant.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
 import cookieParser from "cookie-parser";
+import swaggerUi from "swagger-ui-express";
+import { fileURLToPath } from "url";
+import YAML from "yaml";
 import { Server } from "socket.io";
 import { createServer } from "http";
 import cors from "cors";
+import path from "path";
+import fs from "fs";
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+const filePath = path.resolve(__dirname, "./swagger.yaml");
+const fileContent = fs.readFileSync(filePath, "utf8");
+
+const hostUrl = process.env.ChtazoAPI_HOST_URL || "http://localhost:3000";
+const replacedContent = fileContent?.replace(
+    "- url: ${{server}}",
+    `- url: ${hostUrl}/api/v1/`,
+);
+
+const swaggerDocument = YAML.parse(replacedContent);
 
 const server = createServer(app);
 
@@ -31,7 +48,21 @@ app.use(
         credentials: true,
     }),
 );
-
+/* The code block `app.use(function (req, res, next) { ... });` is setting up CORS (Cross-Origin
+Resource Sharing) headers in your Express application. */
+app.use(function (req, res, next) {
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        "https://chatzo-live.vercel.app",
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "OPTIONS, GET, POST, PUT, PATCH, DELETE",
+    );
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+});
 /* Route Import */
 import { adminRoutes } from "./routes/admin.route.js";
 import { userRoutes } from "./routes/user.route.js";
@@ -42,7 +73,18 @@ app.use(`${BASE_URL}/user`, userRoutes);
 app.use(`${BASE_URL}/chat`, chatRoutes);
 app.use(`${BASE_URL}/admin`, adminRoutes);
 
-
+// * API DOCS
+// ? Keeping swagger code at the end so that we can load swagger on "/" route
+app.use(
+    "/api/v1/docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument, {
+        swaggerOptions: {
+            docExpansion: "none", // keep all the sections collapsed by default
+        },
+        customSiteTitle: "Chatzo API Docs",
+    }),
+);
 app.all("*", (req, res, next) => {
     res.status(404).json({
         message: "Not found",
